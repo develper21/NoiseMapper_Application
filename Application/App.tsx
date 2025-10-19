f7683f397d513ad301cdc18076317c474c9d27be
develper21 <githubnarvin@gmail.com>
2025-10-19 19:17:37 +0530

    add app.tsx file

---
+++ Application/App.tsx
@@ -0,0 +1,7 @@
+import 'expo-router/entry';
+import './global.css';
+
+// For traditional Expo setup (when not using Expo Router)
+export default function App() {
+  return null; // This won't be used when using Expo Router
+}
